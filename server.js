c6ac07c68a3097e3afaf409948d42269941d6f70
AxelZublena <zubl0001@hz.nl>
2021-01-25 22:20:40 +0100

    Updated login and admin routes

--- server.js
+++ server.js
@@ -25,27 +25,17 @@ app.use(
 app.use(fileUpload());
 
 const users = new Datastore("./databases/users.db");
-//const rf2_cars = new Datastore("./databases/rf2_cars.db");
-//const rf2_tracks = new Datastore("./databases/rf2_tracks.db");
 const raceEvents = new Datastore("./databases/raceEvents.db");
 users.loadDatabase();
-//rf2_cars.loadDatabase();
-//rf2_tracks.loadDatabase();
 raceEvents.loadDatabase();
 
 // Load the dashboard if there is a session
 app.get("/dashboard", (request, response) => {
 	if (!request.session.user) {
-		return response.status(401).send();
+		return response.sendFile(path.join(__dirname + "/public/login.html"));
+		//return response.status(401).send();
 	}
 	return response.sendFile(path.join(__dirname + "/dashboard.html"));
-});
-// Load the register page
-app.get("/change-password", (request, response) => {
-	if (!request.session.user) {
-		return response.status(401).send();
-	}
-	return response.sendFile(path.join(__dirname + "/change-password.html"));
 });
 // Load the admin page
 app.get("/admin", (request, response) => {
@@ -65,34 +55,34 @@ app.get("/login", (request, response) => {
 	return response.sendFile(path.join(__dirname + "/public/login.html"));
 });
 
-// Handle registration form
-app.post("/register", async (request, response) => {
-	try {
-		users.find({ username: request.body.username }, async (error, data) => {
-			if (error) {
-				response.end();
-				return 0;
-			}
+//// Handle registration form
+//app.post("/register", async (request, response) => {
+	//try {
+		//users.find({ username: request.body.username }, async (error, data) => {
+			//if (error) {
+				//response.end();
+				//return 0;
+			//}
 
-			if (Object.keys(data).length === 0) {
-				const hashPassword = await bcrypt.hash(request.body.password, 10);
-				const newUser = {
-					username: request.body.username,
-					password: hashPassword
-				};
+			//if (Object.keys(data).length === 0) {
+				//const hashPassword = await bcrypt.hash(request.body.password, 10);
+				//const newUser = {
+					//username: request.body.username,
+					//password: hashPassword
+				//};
 
-				users.insert(newUser);
-				console.log("Registered user:", request.body.username);
-				return response.redirect("login.html");
-			} else {
-				console.log(request.body.username + " is already registered");
-				return response.redirect("login.html");
-			}
-		});
-	} catch {
-		return response.send("Internal server error");
-	}
-});
+				//users.insert(newUser);
+				//console.log("Registered user:", request.body.username);
+				//return response.redirect("login.html");
+			//} else {
+				//console.log(request.body.username + " is already registered");
+				//return response.redirect("login.html");
+			//}
+		//});
+	//} catch {
+		//return response.send("Internal server error");
+	//}
+//});
 
 // Handle login form
 app.post("/login", (request, response) => {
